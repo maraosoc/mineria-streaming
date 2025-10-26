ea3e457e2fd7b60a77905cef0605fa382d42083b
Paula Pirela <37486469+PaulaPirela@users.noreply.github.com>
2025-10-26 18:29:56 -0500

    Add files via upload

--- src/domain.py
+++ src/domain.py
@@ -2,7 +2,16 @@ import datetime
 from typing import NamedTuple, TypedDict
 
 
-class Result(NamedTuple):
+# class Result(NamedTuple):
+#     value: float
+#     newest_considered: datetime.datetime
+#     oldest_considered: datetime.datetime
+
+
+from dataclasses import dataclass
+
+@dataclass
+class Result:
     value: float
     newest_considered: datetime.datetime
     oldest_considered: datetime.datetime
